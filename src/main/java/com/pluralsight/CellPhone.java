96aaca9bc5060a77c6d40042d2ad722f4a1d124b
Michael <michael.elliston95@gmail.com>
2025-10-01 11:59:11 -0500

    added dial method

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -1,4 +1,27 @@
 package com.pluralsight;
 
 public class CellPhone {
+    private String model;
+    private String phoneNumber;
+    public CellPhone() {
+        String model = "";
+        String phoneNumber = "";
+
+    }
+    public String getModel() {
+        return model;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public void dial(String dialNumber) {
+        System.out.printf("A phone classified as \"%s\" is calling %s", model, phoneNumber);
+    }
 }
+
--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,4 +1,22 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class CellPhoneApplication {
+    public static void main(String[] args) {
+        CellPhone c1 = new CellPhone();
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.print("Please input your Phone's Model: ");
+        c1.setModel(myScanner.nextLine());
+
+        System.out.print("\nPlease input your Phone Number: ");
+        c1.setPhoneNumber(myScanner.nextLine());
+
+        System.out.println("What is the Phone's Model?: " + c1.getModel() + "\nWhat is the Phone's Number?: " + c1.getPhoneNumber());
+
+        c1.dial("855-555-2222");
+
+
+    }
 }
