145ce5462e0460105bcc333e0d21f47242d0d280
Michael <michael.elliston95@gmail.com>
2025-10-01 15:33:13 -0500

    removed scanner for simplicity in testing, final print looks good

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -1,12 +1,16 @@
 package com.pluralsight;
 
 public class CellPhone {
-    private String model;
-    private String phoneNumber;
+    public String model;
+    public String phoneNumber;
     public CellPhone() {
         this.model = "";
         this.phoneNumber = "";
 
+    }
+    public CellPhone(String phoneNumber, String model) {
+        model = "";
+        phoneNumber = "";
     }
     public String getModel() {
         return model;
@@ -23,11 +27,14 @@ public class CellPhone {
     public void dial(String dialNumber) {
         System.out.printf("A phone classified as \"%s\" is calling \"%s\".\n", model, dialNumber);
     }
+    public void dial(CellPhone phoneNumber) {
+        System.out.printf("\nA phone classified as \"%s\" is calling \"%s\".\n", model, phoneNumber.getPhoneNumber());
+    }
     public static void display(CellPhone phone) {
         System.out.printf("%s's phone number is %s.\n", phone.getModel(), phone.getPhoneNumber());
     }
-    public void dialDouble(CellPhone modelBeingDialed) {
-        System.out.printf("The %s is calling the %s.", this.model, modelBeingDialed.getModel());
+    public void dialDouble(String model) {
+        System.out.printf("The %s is calling the %s.\n", this.model, model);
     }
 
 }
--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,33 +1,40 @@
 package com.pluralsight;
 
-import java.util.Scanner;
-
 import static com.pluralsight.CellPhone.display;
 
 public class CellPhoneApplication {
     public static void main(String[] args) {
         CellPhone c1 = new CellPhone();
         CellPhone c2 = new CellPhone();
-        Scanner myScanner = new Scanner(System.in);
+        CellPhone c3 = new CellPhone();
 
         System.out.print("Please input your Phone's Model: ");
-        c1.setModel(myScanner.nextLine());
+        c1.setModel("iPhone 15");
 
         System.out.print("\nPlease input your Phone Number: ");
-        c1.setPhoneNumber(myScanner.nextLine());
+        c1.setPhoneNumber("123-456-7890");
 
         System.out.print("\nPlease input the Phone Number you wish to dial: ");
-        c2.setPhoneNumber(myScanner.nextLine());
+        c2.setPhoneNumber("987-654-3210");
+        c2.setModel("Samsung Galaxy");
+        c3.setModel("Landline");
+        c3.setPhoneNumber("543-678-9021");
 
         System.out.println("What is your Phone's Model?: " + c1.getModel() + "\nWhat is your Phone's Number?: " + c1.getPhoneNumber());
 
-        c1.dial("855-555-2222");
+        c1.dial(c3);
 
         display(c1);
         display(c2);
+        display(c3);
 
         c1.dial(c2.getModel());
         c2.dial(c1.getModel());
+        c3.dial(c1.getModel());
+        System.out.println("This is the double: ");
+        c1.dialDouble(c2.getModel());
+
+        c1.dial(c3.getPhoneNumber());
 
 
     }
